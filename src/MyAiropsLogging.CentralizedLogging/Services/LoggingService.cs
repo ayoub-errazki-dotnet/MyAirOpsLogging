68b051e3e35d934837440f2111e1fe258977ed2a
ayoub-errazki-dotnet <ayouberrazkiuk2@gmail.com>
2025-03-23 11:02:09 +0100

    TDD - Implement Logging service

--- src/MyAiropsLogging.CentralizedLogging/Services/LoggingService.cs
+++ src/MyAiropsLogging.CentralizedLogging/Services/LoggingService.cs
@@ -16,12 +16,37 @@ namespace MyAiropsLogging.CentralizedLogging.Services
 
         public string TruncateLog(string message)
         {
-            return string.Empty;
+            if (!string.IsNullOrEmpty(message) && message.Length > 255)
+            {
+                message = message.Substring(0, 252) + "...";
+            }
+            return message;
         }
 
         public void LogMessage(LogMessageDto log)
         {
-            
+            if (log == null)
+                throw new ArgumentException("Log Message is required");
+
+            log.MessageTemplate = TruncateLog(log.MessageTemplate);
+
+            var logFormatted = $" ID : [{log.Id}] | {log.MessageTemplate}";
+
+            switch (log.Level)
+            {
+                case LevelDto.Error:
+                    _logger.Error(logFormatted);
+                    break;
+                case LevelDto.Warning:
+                    _logger.Warning(logFormatted);
+                    break;
+                case LevelDto.Fatal:
+                    _logger.Fatal(logFormatted);
+                    break;
+                default:
+                    _logger.Information(logFormatted);
+                    break;
+            }
         }
     }
 }
