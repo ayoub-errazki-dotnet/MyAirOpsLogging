130af386f2f570c99a80bea0c95742d6ae02f187
ayoub-errazki-dotnet <ayouberrazkiuk2@gmail.com>
2025-03-23 11:29:10 +0100

    Add more unit tests

--- tests/MyAiropsLogging.CentralizedLogging.Tests/LoggingServiceTests.cs
+++ tests/MyAiropsLogging.CentralizedLogging.Tests/LoggingServiceTests.cs
@@ -26,11 +26,45 @@ public class LoggingServiceTests
         _loggerMock.Verify(logger => logger.Information(It.Is<string>(s => s.Contains("Test info"))), Times.Once);
     }
 
+    [Fact]
+    public void LogMessage_ShouldCallLogErrorMessage()
+    {
+        var logMessage = new LogMessageDto { MessageTemplate = "Test error", Level = LevelDto.Error };
+        _loggingService.LogMessage(logMessage);
+
+        _loggerMock.Verify(logger => logger.Error(It.Is<string>(s => s.Contains("Test error"))), Times.Once);
+    }
+
+    [Fact]
+    public void LogMessage_ShouldCallLogFatalMessage()
+    {
+        var logMessage = new LogMessageDto { MessageTemplate = "Test fatal", Level = LevelDto.Fatal };
+        _loggingService.LogMessage(logMessage);
+
+        _loggerMock.Verify(logger => logger.Fatal(It.Is<string>(s => s.Contains("Test fatal"))), Times.Once);
+    }
+
+    [Fact]
+    public void LogMessage_ShouldCallLogWarningMessage()
+    {
+        var logMessage = new LogMessageDto { MessageTemplate = "Test warn", Level = LevelDto.Warning };
+        _loggingService.LogMessage(logMessage);
+
+        _loggerMock.Verify(logger => logger.Warning(It.Is<string>(s => s.Contains("Test warn"))), Times.Once);
+    }
 
     [Fact]
     public void LogMessage_ShouldThrowExceptionWhenMessageIsNull()
     {
         Action action = () => _loggingService.LogMessage(null);
+        Assert.Throws<ArgumentException>(action);
+    }
+
+    [Fact]
+    public void LogMessage_ShouldThrowExceptionWhenMessageIsEmpty()
+    {
+        var log = new LogMessageDto{MessageTemplate = string.Empty};
+        Action action = () => _loggingService.LogMessage(log);
         Assert.Throws<ArgumentException>(action);
     }
 
